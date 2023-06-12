9008f0bfedfb1e0da80859f003d88b0c43bfc488
root <root@DESKTOP-AU15A0S>
2023-06-12 12:08:49 +0300

    New SQL names

--- store-pages/services/itemService.js
+++ store-pages/services/itemService.js
@@ -2,23 +2,23 @@ import { executeQuery } from "../database/database.js";
 
 const create = async (name, shoppingListId) => {
   await executeQuery(
-    "INSERT INTO shopping_list_items (name, shopping_list_id) VALUES ($name , $shoppingListId);",
+    "INSERT INTO store_page_items (name, store_page_id) VALUES ($name , $shoppingListId);",
     {name: name, shoppingListId: shoppingListId},
   );
 };
 
 const findAllItems = async (shoppingListId) => {
-    const notCollected = await executeQuery("SELECT * FROM shopping_list_items WHERE collected = false AND shopping_list_id = $shoppingListId ORDER BY name ASC;",
+    const notCollected = await executeQuery("SELECT * FROM store_page_items WHERE collected = false AND store_page_id = $shoppingListId ORDER BY name ASC;",
     {shoppingListId: shoppingListId},
     );
-    const collected = await executeQuery("SELECT * FROM shopping_list_items WHERE collected = true AND shopping_list_id = $shoppingListId ORDER BY name ASC;",
+    const collected = await executeQuery("SELECT * FROM store_page_items WHERE collected = true AND store_page_id = $shoppingListId ORDER BY name ASC;",
     {shoppingListId: shoppingListId},
     );
     return { listId: shoppingListId, nonCollected: notCollected, collected: collected };
 };
 
 const countAllItems = async () => {
-    const number = await executeQuery("SELECT COUNT(*) FROM shopping_list_items;");
+    const number = await executeQuery("SELECT COUNT(*) FROM store_page_items;");
     const result = parseInt(number.rows[0].count);
     if (result == undefined || result < 1) {
       return 0;
@@ -29,7 +29,7 @@ const countAllItems = async () => {
 
 
 const collectItem = async (id) => {
-    await executeQuery("UPDATE shopping_list_items SET collected = true WHERE id = $id;",
+    await executeQuery("UPDATE store_page_items SET collected = true WHERE id = $id;",
     {id: id},
     );
 };
--- store-pages/services/listService.js
+++ store-pages/services/listService.js
@@ -1,29 +1,29 @@
 import { executeQuery } from "../database/database.js";
 
 const create = async (name) => {
-  await executeQuery("INSERT INTO shopping_lists (name) VALUES ($name);",
+  await executeQuery("INSERT INTO store_pages (name) VALUES ($name);",
   {name: name},
   );
 };
 
 const deactivateList = async (id) => {
-  await executeQuery("UPDATE shopping_lists SET active = false WHERE id = $id;",
+  await executeQuery("UPDATE store_pages SET active = false WHERE id = $id;",
   {id: id},
   );
 };
 
 const getListName = async (id) => {
-  return await executeQuery("SELECT name FROM shopping_lists WHERE id = $id;",
+  return await executeQuery("SELECT name FROM store_pages WHERE id = $id;",
   {id: id},
   );
 };
 
 const findAllActiveLists = async () => {
-  return await executeQuery("SELECT * FROM shopping_lists WHERE active = true;");
+  return await executeQuery("SELECT * FROM store_pages WHERE active = true;");
 };
 
 const countAllLists = async () => {
-  const number = await executeQuery("SELECT COUNT(*) FROM shopping_lists;");
+  const number = await executeQuery("SELECT COUNT(*) FROM store_pages;");
   const result = parseInt(number.rows[0].count);
   if (result == undefined || result < 1) {
     return "No shopping lists yet.";
