e825e63ccd84901875df8e2759b0186048c40b5a
root <root@DESKTOP-AU15A0S>
2023-06-19 15:21:16 +0300

    Back to previous app.js structure(2)

--- store-pages/app.js
+++ store-pages/app.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
 import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
-import * as listController from "./controllers/listController.js";
+import * as pageController from "./controllers/pageController.js";
 import * as itemController from "./controllers/itemController.js";
 import * as mainController from "./controllers/mainController.js";
 
@@ -13,17 +13,17 @@ const handleRequest = async (request) => {
 
   if (url.pathname === "/" && request.method === "GET") {
     return await mainController.viewMain(request);
-  } else if (url.pathname === "/lists" && request.method === "POST") {
-    return await listController.addList(request);
-  } else if (url.pathname === "/lists" && request.method === "GET") {
-    return await listController.viewLists(request);
-  } else if (url.pathname.match("lists/[0-9]+/deactivate") && request.method === "POST") {
-    return await listController.deactivateList(request);
-  } else if (url.pathname.match("lists/[0-9]+") && request.method === "GET") {
-    return await itemController.viewListItems(request);
-  } else if (url.pathname.match("lists/[0-9]+/items" ) && request.method === "POST") {
+  } else if (url.pathname === "/pages" && request.method === "POST") {
+    return await pageController.addPage(request);
+  } else if (url.pathname === "/pages" && request.method === "GET") {
+    return await pageController.viewPages(request);
+  } else if (url.pathname.match("pages/[0-9]+/deactivate") && request.method === "POST") {
+    return await pageController.deactivatePage(request);
+  } else if (url.pathname.match("pages/[0-9]+") && request.method === "GET") {
+    return await itemController.viewPageItems(request);
+  } else if (url.pathname.match("pages/[0-9]+/items" ) && request.method === "POST") {
     return await itemController.addItem(request);
-  } else if (url.pathname.match("lists/[0-9]+/[0-9]+/collect") && request.method === "POST") {
+  } else if (url.pathname.match("pages/[0-9]+/[0-9]+/collect") && request.method === "POST") {
     console.log("collect");
     return await itemController.collectItem(request);
   } else {
